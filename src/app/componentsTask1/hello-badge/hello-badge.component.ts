43135123a6b19eec3ce61984ca0f749bbbcc306b
Serey Phem <serey.phem.12022006@gmail.com>
2026-09-01 08:25:47 +0700

    remove component file

--- src/app/componentsTask1/hello-badge/hello-badge.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  imports: [],
-  selector: 'app-hello-badge',
-  styleUrl: './hello-badge.component.css',
-  templateUrl: './hello-badge.component.html',
-})
-export class HelloBadgeComponent {}

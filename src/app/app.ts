3ac7e2429dfd71641db3b1cacde47b8cec0efdb9
Serey Phem <serey.phem.12022006@gmail.com>
2026-09-01 15:11:21 +0700

    Componet part 2 Content Projection v2

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,15 +1,37 @@
 import { NgIf } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, TemplateRef, viewChild, ViewChild, ViewContainerRef } from '@angular/core';
 import { WeatherComponent } from './components/weather/weather.component';
 import { WeatherContentComponent } from './components/weather-content/weather-content.component';
 import { WeatherTopBarComponent } from './components/weather-top-bar/weather-top-bar.component';
 import { WeatherBottomDetailComponent } from './components/weather-bottom-detail/weather-bottom-detail.component';
 @Component({
   selector: 'app-root',
-  imports: [NgIf, WeatherComponent, WeatherContentComponent, WeatherTopBarComponent, WeatherBottomDetailComponent],
+  imports: [
+    NgIf,
+    WeatherComponent,
+    WeatherContentComponent,
+    WeatherTopBarComponent,
+    WeatherBottomDetailComponent,
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   isLoggedIn = true;
+
+  @ViewChild('burgerTemplate') burgerTemplate!: TemplateRef<unknown>;
+  @ViewChild('pizzaTemplate') pizzaTemplate!: TemplateRef<unknown>;
+
+  @ViewChild('container', { read: ViewContainerRef}) container!: ViewContainerRef;
+
+  showBurger() {
+    this.container.clear();
+    this.container.createEmbeddedView(this.burgerTemplate);
+  }
+
+  showPizza() {
+    this.container.clear();
+    this.container.createEmbeddedView(this.pizzaTemplate);
+  }
+
 }

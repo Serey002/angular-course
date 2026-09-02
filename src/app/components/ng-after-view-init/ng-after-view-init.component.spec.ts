a0ed4ee8cdaab09edd98f86ab27916804186fabb
Serey Phem <serey.phem.12022006@gmail.com>
2026-09-02 21:38:25 +0700

    Angular lifecycle hook v2

---
+++ src/app/components/ng-after-view-init/ng-after-view-init.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { NgAfterViewInitComponent } from './ng-after-view-init.component';
+
+describe('NgAfterViewInitComponent', () => {
+  let component: NgAfterViewInitComponent;
+  let fixture: ComponentFixture<NgAfterViewInitComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [NgAfterViewInitComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(NgAfterViewInitComponent);
+    component = fixture.componentInstance;
+    await fixture.whenStable();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/components/ng-after-view-init/ng-after-view-init.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-ng-after-view-init',
+  styleUrl: './ng-after-view-init.component.css',
+  templateUrl: './ng-after-view-init.component.html',
+})
+export class NgAfterViewInitComponent {}
